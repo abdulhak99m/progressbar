b71af0da0a501148ada4e7615feccf88f45d3c3d
abdulhak99m <abdulhakeemimran@hotmail.com>
2021-07-09 03:03:14 +0100

    q

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCUBBw-jYFjjrV08ZyXhRSXKr4J9XDbbUs",
-  authDomain: "ptachat-c053f.firebaseapp.com",
-  projectId: "ptachat-c053f",
-  storageBucket: "ptachat-c053f.appspot.com",
-  messagingSenderId: "406564631752",
-  appId: "1:406564631752:web:77cac51afe888cb9fca1fd",
-  measurementId: "G-27VFH6LP6Z"
-  };
+  apiKey: "AIzaSyAS6RgbNbxMOfY1RaqeLnlhdmVOqrqE0A8",
+  authDomain: "convo-c6304.firebaseapp.com",
+  databaseURL: "https://convo-c6304-default-rtdb.firebaseio.com",
+  projectId: "convo-c6304",
+  storageBucket: "convo-c6304.appspot.com",
+  messagingSenderId: "894390327963",
+  appId: "1:894390327963:web:8834a57c28eba56507043e",
+  measurementId: "G-ZVL6TC4JTS"
+};
+
   const fire = firebase.initializeApp(firebaseConfig);
 
   export const db = fire.database().ref();
